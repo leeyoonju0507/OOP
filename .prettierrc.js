28321deb8fc7657d2048c1e71ff16a6aae38baee
happiness6533 <happiness6533@gmail.com>
2023-08-20 13:48:13 +0900

    feat: set up dev env

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  htmlWhitespaceSensitivity: 'ignore',
+  printWidth: 100,
+  singleQuote: true,
+  tabWidth: 2,
+};
