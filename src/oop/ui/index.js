f8ee8072177383ad9665eaba110859fda818f254
leeyoonju0507 <leeyoonju0507@gmail.com>
2024-01-07 12:46:08 +0900

    feat: home.html, signup assignment finish

--- src/oop/ui/index.js
+++ src/oop/ui/index.js
@@ -13,7 +13,11 @@ window.addEventListener('load', () => {
   const windowHeight = window.innerHeight;
   const authButtonContainer = document.getElementById('auth-button-container');
   const loginButton = window.document.getElementById('login-button');
+  const signupButton = window.document.getElementById('sign-up-button');
 
+  signupButton.addEventListener('click',(e)=>{
+    window.location.href = 'signup.html';
+  }) 
   // 인증 화면 스타일
   authButtonContainer.style.position = 'absolute';
   authButtonContainer.style.top = `${(windowHeight - authButtonContainer.clientHeight) / 2}px`;
@@ -27,18 +31,19 @@ window.addEventListener('load', () => {
     modalAuthButtonContainer.style.top = `${
       (modalBody.clientHeight - modalAuthButtonContainer.clientHeight) * 0.5
     }px`;
-  });
-
-  // 모달 창
-  const modalWrap = window.document.getElementById('modal-wrap');
-  const modalBody = window.document.getElementById('modal-body');
+  }); 
+  
+  // 로그인 모달 창
+  const modalWrap = window.document.getElementById('login-modal-wrap');
+  const modalBody = window.document.getElementById('login-modal-body');
   const modalAuthButtonContainer = document.getElementById('modal-auth-button-container');
   const idInput = window.document.getElementById('id-input');
   const passwordInput = window.document.getElementById('password-input');
   const loginModal = document.getElementById('login-modal-button');
   const closeModal = window.document.getElementById('close-modal-button');
-
-  // 모달 창 로직
+  
+  
+  // 로그인 모달 창 로직
   idInput.addEventListener('input', (e) => {
     userId = idInput.value;
   });
@@ -53,13 +58,47 @@ window.addEventListener('load', () => {
     idInput.value = '';
     passwordInput.value = '';
   });
-
+  
+  //회원가입 창
+  // const signupForm = window.document.getElementById('sign-up-form');
+  // signupForm.style.position = 'relative';
+  // signupForm.style.top = `${(windowHeight) * 0.5}px`
+  
+  // const checkSignUpButton = window.document.getElementById('check-signup-button');
+  // const signupWrap = window.document.getElementById('signup-modal-wrap');
+  // const signupBody = window.document.getElementById('signup-modal-body');
+  // const closeSignUpModal = window.document.getElementById('close-signupmodal-button')
+  
+  // checkSignUpButton.addEventListener('click',(e)=>{
+  //   signupWrap.style.display = 'block';
+  //   signupBody.style.marginTop = `${(window.innerHeight - modalBody.clientHeight) / 2}px`;
+  // })
+  
+  // closeSignUpModal.addEventListener('click',(e)=>{
+  //   signupWrap.style.display = 'none';
+  // })
+  
   // 상품 창
   const productTitleInput = window.document.getElementById('product-title-input');
   const buyButton = window.document.getElementById('buy-button');
   const addButton = window.document.getElementById('add-button');
   const productList = Array.from(window.document.getElementsByClassName('product'));
   const productContainer = window.document.getElementById('products-container');
+
+  //home.html: 등록하기 클릭시 팝업창
+  const popUpWrap = window.document.getElementById('popup-wrap');
+  const popUpBody = window.document.getElementById('popup-body');
+  productContainer.style.position = 'relative';
+  productContainer.style.top = `${(window.innerHeight) * 0.5}px`;
+  addButton.addEventListener('click',(e)=>{
+    popUpWrap.style.display = 'block';
+    popUpBody.style.marginTop = `${(window.innerHeight - popUpBody.clientHeight) / 2}px`;
+  })  
+  const closePopup = window.document.getElementById('close-popup-button');
+  closePopup.addEventListener('click',(e)=>{
+    popUpWrap.style.display = 'none';
+  })
+
 
   // 상품 창 로직
   for (let i = 0; i < productList.length; i++) {
@@ -102,4 +141,8 @@ window.addEventListener('load', () => {
       addProductIndex(productContainer.children.length - 1);
     });
   });
+
+  
+
+
 });
