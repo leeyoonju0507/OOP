dadf4cab76230fb5303961e0ce7aa3ae5f71f9c3
happiness6533 <happiness6533@gmail.com>
2024-01-13 14:05:18 +0900

    feat: 세팅 끝

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  arrowParens: 'always',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  htmlWhitespaceSensitivity: 'ignore',
-  printWidth: 120,
-  singleQuote: true,
-  tabWidth: 2,
-};
